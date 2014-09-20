79d25b36ebefada94e33c7380a32625ad1053950
znik <nik.zaborovsky@gmail.com>
2014-09-19 22:17:28 -0700

    Added scoping function

--- scoping.h
+++ scoping.h
@@ -8,11 +8,20 @@
 struct scoping {
 	enum {NO_END_LINE = -1};
 	bool init(const std::vector<std::string>& /*srcfiles*/);
-	inline int endline(const std::string& file, int startline) const {
+	int endline(const std::string& file, int startline) const {
 		assert(scope_t() != _scopes.at(file) && "Scoping: no file");
 		if (0 == _scopes.at(file).at(startline))
 			return NO_END_LINE;
 		return _scopes.at(file).at(startline);
+	}
+	// 'scope' is a lexical scope which includes declline and which
+	// left end is the closest to the declaration of all file scopes.
+	std::pair<int, int> scope(const std::string& file, int declline) {
+		const scope_t& sc = _scopes[file];
+		for(auto i = sc.rbegin(); sc.rend() != i; ++i) {
+			if (i->first > declline || i->second < declline) continue;
+			return *i;
+		}
 	}
 private:
 	typedef std::map<int, int> scope_t;
